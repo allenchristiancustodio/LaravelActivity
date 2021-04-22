60c5d30e63d3fb0e9bbda8bd3c7e18c6d37e858f
allencustodio <53993336+allencustodio@users.noreply.github.com>
2021-04-23 04:20:13 +0800

    ok

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\post;
+use Illuminate\Http\Request;
+use SebastianBergmann\Environment\Console;
+
+use Illuminate\Support\Facades\Auth;
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       //return posts view
+        $post = Post::get();
+        return view('posts.index', compact('post'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        $user_id = Auth::id();
+        if ($user_id) {
+            return view('posts.create');
+        } else {
+            return view('posts.alert');
+        }
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post ->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //show data
+        $post = Post::find($id);
+        return view('posts.show',compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+        $user_id = Auth::id();
+        if ($user_id) {
+            $post = Post::find($id);
+            return view('posts.edit', compact('post'));
+        } else {
+            return view('posts.alert');
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //update the value
+        $post = Post::find($id);
+        $post -> title = $request->title;
+        $post ->description = $request->description;
+        $post ->save();
+
+        return redirect('/posts');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       //delete data id user is logged in
+       $user_id = Auth::id();
+       if ($user_id) {
+           $post = Post::find($id);
+           $post->delete();
+           return redirect('/posts');
+       } else {
+           return view('posts.alert');
+       }
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <a class="btn button btn-info" href="/posts/create">Create New</a>
+                    <div class="card-body">
+
+
+                        <table class="table">
+                            <thead>
+                                    <tr>
+                                        <th> ID </th>
+                                        <th> TITLE </th>
+                                        <th> DESCRIPTION </th>
+                                        <th>  </th>
+                                    </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($post as $post)
+                                <tr>
+                                        <td> {{ $post->id }} </td>
+                                        <td> {{ $post->title }} </td>
+                                        <td> {{ $post->description }} </td>
+                                        <td> <a  href="/posts/{{$post->id}}" class="btn btn-success"> View </a> </td>
+                                        <td> <a  href="/posts/{{$post->id}}/edit" class="btn btn-info"> Edit </a> </td>
+                                        <td>
+                                            <form action="{{route('posts.destroy', $post ->id)}}" method="POST">
+                                                @method('DELETE')
+                                                @csrf
+
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
